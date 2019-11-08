1196abbce6066cba7e46b90016c35a39f7bced2b
Isaac Muñoz San Andres <imunosan@everis.com>
2019-11-08 12:20:58 +0100

    Funciones JS añadidas

--- gulp/config.js
+++ gulp/config.js
@@ -64,8 +64,8 @@
             },
             libs: {
                 requiredJS: [
-                    './node_modules/requirejs/require.js',
-                    './node_modules/jquery/src/jquery.js',
+                   // './node_modules/requirejs/require.js',
+                    './node_modules/jquery/dist/jquery.min.js',
                     './node_modules/slick-carousel/slick/slick.min.js'
                 ],
                 requiredCSS: [
